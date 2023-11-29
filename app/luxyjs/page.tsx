6600c383479de70f5ed64d02dd0e6324a720ed5e
SAWA Nanako <sawa.nanako@gmail.com>
2023-11-29 17:57:52 +0900

    heading等のテキストを追加 #1

--- app/luxyjs/page.tsx
+++ app/luxyjs/page.tsx
@@ -7,22 +7,36 @@ import luxy from 'luxy.js'
 import ShiroNyanImage from "../../public/images/shiro_nyan.png"
 import KuroNyanImage from "../../public/images/kuro_nyan.png"
 import ToraNyanImage from "../../public/images/tora_nyan.png"
+import { RocknRoll_One } from 'next/font/google' 
 
+const TitleFont = RocknRoll_One({
+    weight: '400',
+    preload: false
+})
 const Page = () => {
     useEffect(() => {
         luxy.init();
     }, [])
     return <div className="container w-screen">
-        <main id="luxy" className="h-max w-screen place-content-center bg-gradient-to-b from-cyan-50 to-yellow-500">
-            <div>
-                <h1 className="w-5/6 bg-red-700 p-8 text-3xl">Luxy.jsを使ってみるテストだよ！</h1>
+        <main id="luxy" className="h-max w-screen place-content-center bg-gradient-to-b from-cyan-400 via-yellow-200 to-yellow-200">
+            <div className="luxy-el mt-60 py-10" data-horizontal="1" data-speed-x="-150" data-speed-y="60">
+                <h1 className="w-screen inline-block bg-red-700 p-8 my-10 rotate-[-12deg] text-7xl text-right">
+                    <span className={TitleFont.className}>Luxy.jsを使ってみるテストだよ!</span>
+                </h1>
             </div>
-            <div>
-                <Image src={ShiroNyanImage} alt="楽しそうな白猫" className="luxy-el mx-auto" data-horizontal="1" data-speed-x="-110" data-speed-y="-10" data-offset="1024" width="512" height="512" />
-                <Image src={KuroNyanImage} alt="キメキメの黒猫" className="luxy-el mx-auto" data-horizontal="1" data-speed-x="115" data-speed-y="120" data-offset="-1024" width="512" height="512" />
-                <Image src={ToraNyanImage} alt="しゃきっとしたトラ猫" className="luxy-el mx-auto"  data-speed-y="-70" data-offset="500" width="512" height="512" />
+            <div className="luxy-el py-10" data-horizontal="1" data-speed-x="-150" data-speed-y="60" data-offset="-115">
+                <p className="w-5/6 inline-block bg-amber-600 p-8 my-10 rotate-[-11deg] text-6xl text-right">
+                    <span className={TitleFont.className}>Let's scroll down!</span>
+                </p>
             </div>
-            <Link href="/" className="underline">Homeへ戻る</Link>
+            <div className="flex mt-52">
+                <Image src={KuroNyanImage} alt="キメキメの黒猫" className="luxy-el flex-none" data-horizontal="1" data-speed-x="55" data-speed-y="-5" data-offset="-10" width="512" height="512" />
+                <Image src={ToraNyanImage} alt="しゃきっとしたトラ猫" className="luxy-el flex-none"  data-speed-y="-10" data-offset="0" width="512" height="512" />
+                <Image src={ShiroNyanImage} alt="楽しそうな白猫" className="luxy-el flex-none" data-horizontal="1" data-speed-x="-55" data-speed-y="-5" data-offset="10" width="512" height="512" />
+            </div>
+            <div className="text-center p-4 inline-block align-bottom w-screen">
+                <Link href="/" className="underline text-gray-800">Homeへ戻る</Link>
+            </div>
         </main>
     </div>
 }
